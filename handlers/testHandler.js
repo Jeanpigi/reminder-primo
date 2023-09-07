f000935ff33bd0ea60b7e133cfde375ae87918d3
Jeanpigi <stick-master666@hotmail.com>
2023-09-07 08:38:40 -0500

    [Add] testing in the handler for check functionatly

--- handlers/testHandler.js
+++ handlers/testHandler.js
@@ -5,6 +5,22 @@ const testHandler = async (req, res) => {
     const todayAgendas = await getAgendasForToday();
     console.log("Agendas para el día actual:");
     console.log(todayAgendas);
+    if (todayAgendas.length === 0) {
+      console.log("No hay agendas para hoy.");
+      return; // No hace nada si no hay agendas
+    }
+
+    todayAgendas.forEach((item) => {
+      console.log(
+        "------------------------------------------------------------"
+      );
+      console.log(
+        `Fecha a enviar: ${item.Fechaini} y celular: ${item.Celular}`
+      );
+      console.log(
+        "-------------------------------------------------------------"
+      );
+    });
     res.send("Probando a ver si trae las agendas");
   } catch (error) {
     console.error("Error al obtener las agendas para el día actual:", error);
