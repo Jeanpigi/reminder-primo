b06bafbcac8c798eae1d4521a3f8c49fbb7fcd21
Jeanpigi <stick-master666@hotmail.com>
2024-02-10 11:41:55 -0500

    [Change] route test for agendas and manages routes not found it

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,24 @@ const express = require("express");
 const router = express.Router();
 const sendSMSHandler = require("../handlers/sendSMSHandler");
 const sendWhatsAppHandler = require("../handlers/sendWhatsAppHandler");
-const testHandler = require("../handlers/testHandler");
+const agendasHandler = require("../handlers/agendasHandler");
 
+// Rutas
 router.get("/", (req, res) => {
   res.send("Reagendamiento el primo desde vercel!");
 });
-
-router.get("/test", testHandler);
-
+router.get("/agendas", agendasHandler);
+// Rutas para enviar SMS y WhatsApp
 router.post("/api/sendSMS", sendSMSHandler);
 router.post("/api/sendWhatsApp", sendWhatsAppHandler);
+// Middleware para manejar rutas no encontradas (404)
+router.use((req, res) => {
+  res.status(404).send("La ruta solicitada no existe.");
+});
+// Middleware de manejo de errores
+router.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor.");
+});
 
 module.exports = router;
