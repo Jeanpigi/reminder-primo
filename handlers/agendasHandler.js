21574b12cf034bdd48ac37a9d91d73bcc9e56499
Jeanpigi <stick-master666@hotmail.com>
2024-02-27 09:22:32 -0500

    [Add] change route of agendas /agendas

--- handlers/agendasHandler.js
+++ handlers/agendasHandler.js
@@ -3,30 +3,25 @@ const getAgendasForToday = require("../database/db");
 const agendasHandler = async (req, res) => {
   try {
     const todayAgendas = await getAgendasForToday();
-    console.log("Agendas para el día actual:");
-    console.log(todayAgendas);
-
     if (todayAgendas.length === 0) {
       // Si no hay agendas, envía un mensaje indicándolo
-      console.log("No hay agendas para hoy.");
       res.send("No hay agendas para el día de hoy.");
     } else {
       // Si hay agendas, crea una representación en cadena de ellas
       let agendasString = todayAgendas
         .map(
           (item) =>
-            `Fecha a enviar: ${item.Fechaini}, fecha fin: ${item.Fechafin}, celular: ${item.Celular}`
+            ` Proveedor : ${item.Proveedor}, Fecha a enviar: ${item.Fechaini}, Celular: ${item.Celular}`
         )
         .join("\n");
-
-      console.log(agendasString);
       // Envía la cadena de agendas como respuesta
-      res.send(`Agendas para hoy:\n${agendasString}`);
+      res.send(`Agendas para hoy:[\n${agendasString}]`);
     }
   } catch (error) {
     // Si hay un error, envía un mensaje de error
-    console.error("Error al obtener las agendas para el día actual:", error);
-    res.status(500).send("Error al obtener las agendas para el día de hoy.");
+    res
+      .status(500)
+      .send("Error al obtener las agendas para el día de hoy." + error);
   }
 };
 
